ca89f90791820f8d4be2e444ea056189cdbd3220
Sigma <mukae.yuta.0811@gmail.com>
2025-09-10 14:29:06 +0900

    完成！

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,14 @@
 use std::io;
-use rand::Rng;
 use std:: cmp::Ordering;
+use rand::Rng;
+
 
 fn main() {
     println!("Guess the number!"); // 数を当ててごらん
 
     let serect_number = rand::thread_rng().gen_range(1..101);
 
-    println!("The secret number is {}", serect_number);
+    // println!("The secret number is {}", serect_number);
     
     loop {
         println!("Please input your guess."); // ほら、予想を入力してね
@@ -18,16 +19,20 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line"); // 行の読み込みに失敗しました
 
-        let guess: u32 = guess.trim().parse()
-            .expect("Please type a number!"); // 数値を入力してください！
+        let guess: u32 = match guess.trim().parse() {
+            // .expect("Please type a number!"); // 数値を入力してください！
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+            
         println!("You guessed: {}", guess); // 次のように予想しました: {}
 
         match guess.cmp(&serect_number) {
-        Ordering::Less => println!("Small!"), // 小さすぎ！
-        Ordering::Greater => println!("Big!"), // 大きすぎ！
-        Ordering::Equal => {
-            println!("You Win!"); // やったね！
-            break;
+            Ordering::Less => println!("Small!"), // 小さすぎ！
+            Ordering::Greater => println!("Big!"), // 大きすぎ！
+            Ordering::Equal => {
+                println!("You Win!"); // やったね！
+                break;
             }
         }
     }
