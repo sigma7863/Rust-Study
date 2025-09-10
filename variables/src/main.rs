0e575cdbe5b63e05ca674ae9b6ed0f174af11388
Sigma <mukae.yuta.0811@gmail.com>
2025-09-10 15:53:19 +0900

    3.1 シャドーイングについて

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {}", x); // xの値は{}です
-    x = 6;
+    let x = 5;
+    
+    let x = x + 1;
+    
+    {
+        let x = x * 2;
+        println!("The value of x is: {}", x); // xの値は{}です
+    }
+    
     println!("The value of x is: {}", x);
 }
