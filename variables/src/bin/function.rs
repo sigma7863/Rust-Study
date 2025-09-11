e99568f3f06e6229812c2729d09a2dc5f96d00f6
Ido <mukae.yuta.0811@gmail.com>
2025-09-12 08:47:21 +0900

    3.3 関数本体は、文と式を含む

---
+++ variables/src/bin/function.rs
@@ -0,0 +1,31 @@
+// fn main() {
+//   println!("Hello world!");
+
+//   another_function();
+// }
+
+// fn another_function() {
+//   println!("Another function."); // 別の関数
+// }
+
+// 関数に引数を持たせる
+// fn main() {
+//   another_function(5);
+// }
+
+// fn another_function(x: i32) {
+//   println!("The value of x is: {}", x); // xの値は{}です
+// }
+
+// 複数の型を持たせる
+// fn main() {
+//   print_labeled_measurement(5, 'h');
+// }
+
+// fn print_labeled_measurement(value: i32, unit_label: char) {
+//   println!("The measurement is: {}{}", value, unit_label);
+// }
+
+fn main() {
+    let x = (let y = 6);
+}
