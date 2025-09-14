c157416ae4edb5e56c9f8c7dd158fa42a254220f
Sigma <mukae.yuta.0811@gmail.com>
2025-09-14 18:17:17 +0900

    フォルダ・ファイル名を変更

---
+++ 3-variables/src/bin/3_4-comment.rs
@@ -0,0 +1,17 @@
+// hello, world
+
+// So we’re doing something complicated here, long enough that we need
+// multiple lines of comments to do it! Whew! Hopefully, this comment will
+// explain what’s going on.
+// ここで何か複雑なことをしていて、長すぎるから複数行のコメントが必要なんだ。
+// ふう！願わくば、このコメントで何が起きているか説明されていると嬉しい。
+
+fn main() {
+    let lucky_number = 7; // I’m feeling lucky today(今日はラッキーな気がするよ)
+}
+
+// fn main() {
+//     // I’m feeling lucky today
+//     // 今日はラッキーな気がするよ
+//     let lucky_number = 7;
+// }
