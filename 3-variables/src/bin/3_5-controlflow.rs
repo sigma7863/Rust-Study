b5c102a360adc7ddb37a53e6fc232d8b847b97e3
Ido <mukae.yuta.0811@gmail.com>
2025-09-15 01:29:31 +0900

    3.5 let文内でif式を使う

--- 3-variables/src/bin/3_5-controlflow.rs
+++ 3-variables/src/bin/3_5-controlflow.rs
@@ -18,6 +18,51 @@
   //   }
 // }
 
+// 条件式に論理値を与えればエラーが起きない
+// fn main() {
+//    let number = 3;
+
+//    if number != 0 {
+//      println!("number was something other than zero"); // 数値は0以外の何かです
+//    }
+// }
+
+// else ifで複数の条件式を書ける
+// fn main() {
+//     let number = 6;
+
+//     if number % 4 == 0 {
+//         // 数値は4で割り切れます
+//         println!("number is divisible by 4");
+//     } else if number % 3 == 0 {
+//         // 数値は3で割り切れます
+//         println!("number is divisible by 3");
+//     } else if number % 2 == 0 {
+//         // 数値は2で割り切れます
+//         println!("number is divisible by 2");
+//     } else {
+//         // 数値は4、3、2で割り切れません
+//         println!("number is not divisible by 4, 3, or 2");
+//     }
+// }
+
+// letでif式を定義できる
+// fn main() {
+//    let condition = true;
+//    let number = if condition { 5 } else { 6 };
+
+//    println!("The value of number is: {}", number); // numberの値は、{}です
+// }
+
+// 条件式の型がそれぞれ違うとエラーが起きる
+// fn main() {
+//     let condition = true;
+
+//     let number = if condition { 5 } else { "six" };
+
+//     println!("The value of number is: {}", number);
+// }
+
 // loopでコードを繰り返す
 // fn main() {
 //     loop {
@@ -26,24 +71,24 @@
 // }
 
 // breakで終了, continueでスキップ
-fn main() {
-    let mut count = 0;
-    'counting_up: loop {
-        println!("count = {}", count);
-        let mut remaining = 10;
+// fn main() {
+//     let mut count = 0;
+//     'counting_up: loop {
+//         println!("count = {}", count);
+//         let mut remaining = 10;
 
-        loop {
-            println!("remaining = {}", remaining);
-            if remaining == 9 {
-                break;
-            }
-            if count == 2 {
-              break 'counting_up;
-            }
-            remaining -= 1;
-      }
+//         loop {
+//             println!("remaining = {}", remaining);
+//             if remaining == 9 {
+//                 break;
+//             }
+//             if count == 2 {
+//               break 'counting_up;
+//             }
+//             remaining -= 1;
+//       }
 
-      count += 1;
-    }
-    println!("End count = {}", count);
-}
+//       count += 1;
+//     }
+//     println!("End count = {}", count);
+// }
